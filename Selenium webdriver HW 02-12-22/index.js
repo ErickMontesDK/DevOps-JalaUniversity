069de263191a1fe03d4da997633b4fca112431db
ErickDKnight <erickmontes_76@hotmail.com>
2022-12-04 14:56:31 -0600

    added selenium practice

---
+++ Selenium webdriver HW 02-12-22/index.js
@@ -0,0 +1,14 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+async function searching() {
+  let driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get('https://www.thingiverse.com/');
+    await driver
+    .findElement(By.className('SearchResult__searchResultItem--c4VZk'))
+    .findElement(By.className('ThingCardBody__cardBodyWrapper--ba5pu'))
+    .click();
+}
+searching();
+
+//ThingCardBody__cardBodyWrapper--ba5pu
